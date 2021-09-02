59c24a89b4e4cb70e878e7d66677c9a0ecd03828
ManpreetBhatti <manpreet@bhatti.net>
2021-09-01 21:42:08 -0400

    Update

--- genfiles/scss/index.js
+++ genfiles/scss/index.js
@@ -1,9 +1,9 @@
-import { writeFile } from "fs/promises";
+const fs = require("fs/promises");
 
-export default function () {
-  return writeFile(
+module.exports = function () {
+  return fs.writeFile(
     "./src/scss/_env.scss",
     `$primary: #ce2029;
     $font: "Gilroy", sans-serif;`
   );
-}
+};
