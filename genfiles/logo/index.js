f5aa083ebaac3e8ad988c5811ae2d592c279d7fe
ManpreetBhatti <manpreet@bhatti.net>
2021-09-04 17:30:53 -0400

    Add new images and genfile for fonts and logo

--- genfiles/logo/index.js
+++ genfiles/logo/index.js
@@ -2,11 +2,10 @@ const fs = require("fs/promises");
 const path = require("path");
 
 module.exports = function () {
-  // Change ${logo} with name of logo file and also add logo into this folder
-  return fs.readFile(path.join(__dirname, "${logo}")).then(async (buf) => {
+  return fs.readFile(path.join(__dirname, "./logo.svg")).then(async (buf) => {
     await fs.writeFile(
-      "./src/images/${logo}",
-      buf.toString().replace(/\$COLOR/g, "#ce2029")
+      "./src/images/logo.svg",
+      buf.toString().replace(/\$COLOR/g, "#ce2029"),
     );
   });
 };
