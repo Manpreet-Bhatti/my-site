dee4539c0bed1904ccd4c7c62b61a6b29bd042d0
ManpreetBhatti <manpreet@bhatti.net>
2021-09-04 17:31:27 -0400

    Added data file for contacts and skills

---
+++ src/sections/Contact/data.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const ContactInfo = [
+  {
+    title: <>LinkedIn</>,
+    content: <>manpreet1bhatti</>,
+    link: "https://www.linkedin.com/in/manpreet1bhatti",
+  },
+  {
+    title: <>GitHub</>,
+    content: <>Manpreet-Bhatti</>,
+    link: "https://github.com/Manpreet-Bhatti",
+  },
+  {
+    title: <>Email</>,
+    content: <>manpreet@bhatti.net</>,
+    link: "mailto:manpreet@bhatti.net",
+  },
+];
